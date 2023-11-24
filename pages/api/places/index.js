77f1138d5c40125923490601479f438ce2429ae1
Rikke Saust <rlsaust@gmail.com>
2023-11-24 12:04:24 +0100

    create a new document to the database

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -5,9 +5,20 @@ export default async function handler(request, response) {
   await dbConnect();
   if (request.method === "GET") {
     const places = await Place.find();
-    console.log(places);
+    if (!places) {
+      return response.status(405).json({ message: "GET method not allowed" });
+    }
     return response.status(200).json(places);
-  } else {
-    return response.status(405).json({ message: "GET method not allowed" });
+  }
+  
+  if (request.method === 'POST') {
+    try {
+      const place = request.body;
+      await Place.create(place)
+      console.log('new place:',place);
+      response.status(201).json({status: 'Place created'})
+    } catch (error){
+      response.status(400).json({error: error.message})
+    }
   }
 }
